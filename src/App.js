1961997032b8e42b1ab3aa6ff51e0b14527f2b72
virgiawansaputra <virgiawan267@gmail.com>
2020-06-09 21:46:53 +0700

    props pada reactjs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Home from './Component/Fungsional/Home';
-import Beranda from './Component/Class/Beranda';
+//import Beranda from './Component/Class/Beranda';
 
 function App() {
   return (
     <div className="App">
       <Home/>
-      <Beranda/>
     </div>
   );
 }
--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import Blog from './Blog'
 
 const Home = () => {
-    return <div><h1>Ini adalah Home</h1></div>
+    return <div>
+        <Blog 
+        tanggal="09 Juni 2020"
+        judul="Teknologi Blockchain"
+        summary="Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator."/>
+        <Blog 
+        tanggal="10 Juni 2020"
+        judul="Teknologi Internet"
+        summary="Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator."/>
+        <Blog 
+        tanggal="11 Juni 2020"
+        judul="Teknologi Reactjs"
+        summary="Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator."/>
+
+    </div>
 }
 
 export default Home;
