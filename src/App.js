800568e21bfbdf10b16de6370164c878911e5986
virgiawansaputra <virgiawan267@gmail.com>
2020-06-13 00:28:30 +0700

    implementasi reactstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Parent from './Component/Class/Parent';
+//import Parent from './Component/Class/Parent';
 //import logo from './logo.svg';
 //import './App.css';
 //import Home from './Component/Fungsional/Home';
 //import Beranda from './Component/Class/Beranda';
+import BootstrapComp from './Component/Class/BootstrapComp';
 
 function App() {
   return (
     <div className="App">
-     <Parent />
+    <BootstrapComp/>
     </div>
   );
 }
