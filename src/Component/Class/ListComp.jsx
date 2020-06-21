feec1a5de48d7b8edefcaa524f103df34c41bdae
virgiawansaputra <virgiawan267@gmail.com>
2020-06-22 00:13:13 +0700

    DELETE data REST API pada React.Js

--- src/Component/Class/ListComp.jsx
+++ src/Component/Class/ListComp.jsx
@@ -25,11 +25,43 @@ class ListComp extends PureComponent {
         })
     }
 
+    Deletemahasiswa = (idmahasiswa) => {
+        const {mahasiswa} = this.state
+        const data = qs.stringify({
+            id_mahasiswa: idmahasiswa
+        })
+        
+        axios.delete(api+'/hapus',
+            {
+                data: data,
+                headers: {'Content-type': 'application/x-www-form-urlencoded'}
+            }
+        ).then(json => {
+            if(json.data.status === 200){
+                this.setState({
+                    response: json.data.values,
+                    mahasiswa: mahasiswa.filter(mahasiswa => mahasiswa.id_mahasiswa !== idmahasiswa),
+                    display: 'block'           
+                })
+                // this.props.history.push('/mahasiswa')
+            }else {
+                this.setState({
+                    response: json.data.values,
+                    display: 'block'           
+                })
+                // this.props.history.push('/mahasiswa')
+            }
+        })
+    }
+
 
     render() {
         return (
             <Container>
                 <h2>Data Mahasiswa</h2>
+                <Alert color="success"  style={{display: this.state.display}}>
+                    {this.state.response}
+                </Alert>  
                 <NavLink href="/mahasiswa/tambah"><Button color="success">Tambah Data</Button></NavLink>
                 <hr/>
                 <Table className="table-bordered">
@@ -61,6 +93,9 @@ class ListComp extends PureComponent {
                                 }>
                                     <Button>Edit</Button>
                                 </Link>
+                                <span> </span>
+                                <Button onClick={()=>this.Deletemahasiswa(mahasiswa.id_mahasiswa)} color="danger">Hapus</Button>
+
                             </td>
                         </tr>
                         
