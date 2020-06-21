6d7a27717fa32a83511394298827b90fe684236a
virgiawansaputra <virgiawan267@gmail.com>
2020-06-21 19:31:10 +0700

    React Router dengan Passing Data

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import NavbarComp from './Component/Fungsional/NavbarComp';
 import HomePage from './Component/Fungsional/HomePages';
 import About from './Component/Fungsional/AboutComp'
+import DetailComp from './Component/Fungsional/DetailComp';
 
 const App = () => {
   return (
@@ -17,6 +18,8 @@ const App = () => {
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/about" component={About} />
+      <Route exact path="/detail/:id" component={DetailComp} />
+
     </Switch>
     </BrowserRouter>
   );
--- src/Component/Fungsional/AboutComp.jsx
+++ src/Component/Fungsional/AboutComp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron, Button, Container, Row, Col } from 'reactstrap';
+import CardComp from './CardComp';
 
 
 function AboutComp() {
@@ -14,6 +15,13 @@ function AboutComp() {
             <Button color="primary">Learn More</Button>
           </p>
         </Jumbotron>
+        <Container>
+          <Row>
+            <Col><CardComp id="1" judul="Belajar React" tanggal="21/06/2020"/></Col>
+            <Col><CardComp id="2" judul="Belajar Phyton" tanggal="22/06/2020"/></Col>
+            <Col><CardComp id="3" judul="Belajar PHP" tanggal="23/06/2020"/></Col>
+          </Row>
+        </Container>
       </div>
     )
 }
